a4d8377c24159407800b66f30e2de2b34fc42bfb
AmrIbrahim41 <Amrhima41@gmail.com>
2025-09-14 02:29:34 +0300

    fix: update package name and vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/elsayed-khaled/', // <-- قم بتغيير هذا إلى اسم المستودع الخاص بك
 })
