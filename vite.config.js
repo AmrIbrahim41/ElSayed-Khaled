b26fa9939286f0e495a3e97878588cf03c5d3990
AmrIbrahim41 <Amrhima41@gmail.com>
2025-09-14 02:46:30 +0300

    Revert "fix: update package name and vite base path"
    This reverts commit a4d8377c24159407800b66f30e2de2b34fc42bfb.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/elsayed-khaled/', // <-- قم بتغيير هذا إلى اسم المستودع الخاص بك
 })
